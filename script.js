6fc7ee02cf329dc2d2e1e583e1e8de6524d1ba42
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-04 20:15:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+// const musicbtn=document.querySelectorAll(".btn");
+
+let s1=new audio();
+s1.src="music.mp4";
+
+let s2=new audio();
+s2.src="music.mp4";
+
+let s3=new audio();
+s3.src="music.mp4";
+
+let s4=new audio();
+s4.src="music.mp4";
+
+let s5=new audio();
+s5.src="music.mp4";
+
+let s6=new audio();
+s6.src="music.mp4";

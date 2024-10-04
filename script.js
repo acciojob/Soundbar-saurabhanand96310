5b68ec6102491b7c7ff5673acf5857069dce2135
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-04 20:23:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 //your JS code here. If required.
 // const musicbtn=document.querySelectorAll(".btn");
+let s1 = new Audio('sounds/applause.mp3');
+let s2 = new Audio('sounds/boo.mp3');
+let s3 = new Audio('sounds/gasp.mp3');
+let s4 = new Audio('sounds/tada.mp3');
+let s5 = new Audio('sounds/victory.mp3');
+let s6 = new Audio('sounds/wrong.mp3');
 
-let s1=new Audio();
-s1.src="music.mp4";
-
-let s2=new Audio();
-s2.src="music.mp4";
-
-let s3=new Audio();
-s3.src="music.mp4";
-
-let s4=new Audio();
-s4.src="music.mp4";
-
-let s5=new Audio();
-s5.src="music.mp4";
-
-let s6=new Audio();
-s6.src="music.mp4";
+function pauseAll() {
+	s6.pause() 
+		s1.pause() 
+			s2.pause() 
+				s3.pause()
+					s4.pause() 
+						s5.pause()
+}
